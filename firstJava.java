3ebed0d7f17ca70856ecbd43c9d42e1b8d910f22
OrBarsheshet <155074090+OrBarsheshet@users.noreply.github.com>
2023-12-28 14:59:58 +0200

    upload web

---
+++ firstJava.java
@@ -0,0 +1,10 @@
+public class firstJava{
+    //First JAVA application 
+    public static void main(String[] args){
+        System.out.println("|-------------------------|");
+        System.out.println("|My First Java application|");
+        System.out.println("|-------------------------|");
+
+    }
+}
+    
